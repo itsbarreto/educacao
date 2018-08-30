aa663434fdfce0f127754da38183573d4ac82efd
itsbarreto <itsbarreto@gmail.com>
2018-08-30 11:11:31 -0300

    Quantidades de turmas acrescentadas as tabelas.

--- df_inep_utils.py
+++ df_inep_utils.py
@@ -187,3 +187,40 @@ def monta_df_professores_vs17_resu(df):
     dfa['IN_LECIONA_PUBLICA'] = (df.groupby(col_chave)['TP_DEPENDENCIA'].min() < 4).fillna(False).astype(np.int8)
     dfa['IN_LECIONA_PRIVADA'] = (df.groupby(col_chave)['TP_DEPENDENCIA'].max() == 4).fillna(False).astype(np.int8)
     return dfa
+
+
+
+
+
+
+
+def monta_por_tip_ensino(df,tip,dcr,ano):
+    qtds = None
+    if ano < 2015:
+        qtds = df.loc[df.FK_COD_MOD_ENSINO == tip].groupby(['FK_COD_DOCENTE','PK_COD_ENTIDADE'])[['PK_COD_TURMA']].nunique()
+    elif tip == 1:
+        qtds = df.groupby(['CO_PESSOA_FISICA','CO_ENTIDADE'])[['IN_REGULAR']].sum()
+    elif tip == 3:
+        qtds = df.groupby(['CO_PESSOA_FISICA','CO_ENTIDADE'])[['IN_EJA']].sum()
+    qtds.reset_index(inplace=True)
+    qtds.columns = ['CO_PESSOA_FISICA','CO_ENTIDADE',f'NU_QTD_TURMAS_{dcr}_{ano}']
+    return qtds
+
+def monta_por_tip_serie(df,tip,ano,nivel_escola=True):
+    qtds = None
+    series = {
+        'INFANT' : [1,2,3],
+        'MEDIO' : list(range(25,39))
+    }
+    cols_base = []
+    if ano < 2015:
+        cols_base = ['FK_COD_DOCENTE','PK_COD_ENTIDADE'] if nivel_escola else ['FK_COD_DOCENTE']
+        qtds = df.loc[df.FK_COD_ETAPA_ENSINO.isin(series[tip])].groupby(cols_base)[['PK_COD_TURMA']].nunique()
+    else:
+        cols_base = ['CO_PESSOA_FISICA','CO_ENTIDADE'] if nivel_escola else ['CO_PESSOA_FISICA']
+        qtds = df.loc[df.TP_ETAPA_ENSINO.isin(series[tip])].groupby(cols_base)[['ID_TURMA']].nunique()
+
+    cols_base = ['CO_PESSOA_FISICA','CO_ENTIDADE'] if nivel_escola else ['CO_PESSOA_FISICA']
+    qtds.reset_index(inplace=True)
+    qtds.columns =  cols_base + [f'NU_QTD_TURMAS_{tip}_{ano}']
+    return qtds
